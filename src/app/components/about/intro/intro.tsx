4c022e95a49f454d34533d2967c13a63424f4b56
NitteshKhanna <nitteshkhanna19@gmail.com>
2026-02-07 08:53:31 +0530

    Size fix and changing the intro img from 3 to 1

--- src/app/components/about/intro/intro.tsx
+++ src/app/components/about/intro/intro.tsx
@@ -36,7 +36,14 @@ export function Intro() {
   return (
     <div className="intro-container" ref={introRef}>
       <div className="intro-section">
-        <div className="image-container"
+        <div className="image-container single-image">
+          <img
+              className={` ${shouldAnimate ? "swapped" : "initial"}`}
+              src="/images/about/Group 17 Masked.svg"
+              alt="bottom left image"
+            />
+        </div>
+        {/* <div className="image-container"
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}>
             <img
@@ -54,7 +61,7 @@ export function Intro() {
               src="/images/about/Group 18 Masked.svg"
               alt="top right image"
             />
-        </div>
+        </div> */}
         <div className={`intro-text ${shouldAnimate ? "hovered" : "default"}`}>
           <h1>Hi, I am Stew</h1>
           <h5>Hello everyone my name is Stew! I have been in the crypto space since 2019 and been a part of NFTs specifically since 2021. With a masters degree in Science Technological Studies, I am  passionate about building things that stand on three main values: communication, innovation, and community. I’ve worked with a few big names in the space to help grow their personal brands and visions, now it is time to grow my own! I have been a lead role in a few projects that have been very successful in their own ecosystems and hope to continue to bring value in the Abstract ecosystem with Recruits!</h5>
